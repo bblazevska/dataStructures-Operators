97143ca53e38e2bea91299a04f07d7f1a18b6656
kuzeva <kuzeva01@gmail.com>
2021-08-30 16:34:18 +0200

    strings and strings methods

--- script.js
+++ script.js
@@ -53,6 +53,136 @@ const restaurant = {
 };
 
 ///////////////////////////////////////////////////////////////////
+// STRINGS  
+
+// split and join
+console.log('a+very+nice+string'.split('+'));
+console.log('Jonas Schmedtmann'.split(' '));
+
+const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+console.log(firstName, lastName);
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+
+  console.log(namesUpper.join(' '));
+}
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('bojana kuzeva');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(20,'+').padEnd(30,'+'));
+console.log('Bojana'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+}
+
+console.log(maskCreditCard(46648521814546));
+console.log(maskCreditCard('466485218145464656'));
+
+//Repeat
+
+const message2 = 'Bad weather... All Departures Delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'✈'.repeat(n)}`);
+}
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+
+const airline = 'TAP Air Portugal';
+// const plane = 'A320';
+
+// console.log(plane[0]);
+// console.log(plane[1]);
+// console.log(plane[0]);
+// console.log('B737'[0]);
+
+// console.log(airline.length);
+// console.log('B737'.length);
+
+// console.log(airline.indexOf('r'));
+// console.log(airline.lastIndexOf('r'));
+// console.log(airline.indexOf('Portugal'));
+
+// console.log(airline.slice(4));
+// console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ')+1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  s==='B' || s ==='E'? console.log('You got the middle seat 😧') : console.log('You got lucky 🎉');
+}
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// fix capitalization in name
+const passenger = 'jOnAS';
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect = passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+// comparing emails
+
+const email = 'hello@jonas.io';
+const loginEmail = '  Hello@Jonas.Io \n';
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+//replacing 
+const priceGB = '288,97£'
+const priceUS = priceGB.replace('£', '$').replace(',','.');
+console.log(priceUS);
+
+const announcement = 'All  passengers come to boarding door 23. Boarding door 23!';
+console.log(announcement);
+console.log(announcement.replaceAll('door', 'gate'));
+
+// Booleans
+
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Airb'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the new airbus family!');
+}
+  
+
+
+
+/*
+///////////////////////////////////////////////////////////////////
 // MAPS: ITERATION
 const question = new Map([
   ['question', 'What is the best programming language in the world?'],
@@ -85,7 +215,7 @@ answer === question.get('correct')? console.log(question.get(true)) : console.lo
 // COnverting map to array
 
 console.log([...question]);
-
+*/
 /*
 ///////////////////////////////////////////////////////////////////
 // MAPS: FUNDAMENTALS
